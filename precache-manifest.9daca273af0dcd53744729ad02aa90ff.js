580d8b399ceb56b9b38837b9f78264a0db799a6c
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2022-02-21 16:37:24 +0100

    Updates

---
+++ precache-manifest.9daca273af0dcd53744729ad02aa90ff.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7c35a3e950b19a6dc28",
+    "url": "/js/vendor.b7c35a3e.js"
+  },
+  {
+    "revision": "b829747a85828b8db3fd",
+    "url": "/js/app.b829747a.js"
+  },
+  {
+    "revision": "b829747a85828b8db3fd",
+    "url": "/css/app.e48b6010.css"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "01078151a394b7f93de323c7a6ffaf54",
+    "url": "/img/icn-dontcare.01078151.svg"
+  },
+  {
+    "revision": "2bf4beb06b2a86aa3e33c07c1241d249",
+    "url": "/img/icn-want.2bf4beb0.svg"
+  },
+  {
+    "revision": "7db08aae873c69b4d46fd992cdb51375",
+    "url": "/img/icn-info.7db08aae.svg"
+  },
+  {
+    "revision": "5009d7cfaeba6ca34fc5e9cb524dbafe",
+    "url": "/img/icn-back.5009d7cf.svg"
+  },
+  {
+    "revision": "1976c45280fb94505b71ee4c71a1bbc1",
+    "url": "/img/resultmap-ireland.1976c452.svg"
+  },
+  {
+    "revision": "ce05225409319d79eba74886cf0fa830",
+    "url": "/img/icn-share-twitter.ce052254.svg"
+  },
+  {
+    "revision": "23fdd94ec3db54c4036a195032ca3e3c",
+    "url": "/img/img-innovation.23fdd94e.jpg"
+  },
+  {
+    "revision": "a7a891f674f2def77bdd17558a6c009c",
+    "url": "/img/icn-share-whatsapp.a7a891f6.svg"
+  },
+  {
+    "revision": "b3b1d310ca6a10515691fee484185b73",
+    "url": "/img/infographic.b3b1d310.svg"
+  },
+  {
+    "revision": "ac2a81ad933138192c35bd4bcdb7ca0f",
+    "url": "/img/icn-share-facebook.ac2a81ad.svg"
+  },
+  {
+    "revision": "fbe132882d2aa0f606acfe2c6e3e9d59",
+    "url": "/img/resultmap-greece.fbe13288.svg"
+  },
+  {
+    "revision": "8e5f9de582b2d2986f66c80625c6db44",
+    "url": "/img/logo-countrymatcher.8e5f9de5.svg"
+  },
+  {
+    "revision": "39664a08b813b1e0f9d3929efbc7b3e9",
+    "url": "/img/resultmap-fallback.39664a08.svg"
+  },
+  {
+    "revision": "8593b89864d8fc932cfd4c0cff276561",
+    "url": "/img/resultmap-italy.8593b898.svg"
+  },
+  {
+    "revision": "24ae103a162d154cc616431eeb9e7942",
+    "url": "/index.html"
+  }
+];
